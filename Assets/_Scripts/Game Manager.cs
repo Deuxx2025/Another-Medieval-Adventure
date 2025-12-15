500c9255886a0637cc939d9edf469b20ed0c144f
Deuxx2025 <cristhianloa15@gmail.com>
2025-12-15 14:07:36 +0000

    Basic highlight movement

--- Assets/_Scripts/Game Manager.cs
+++ Assets/_Scripts/Game Manager.cs
@@ -1,7 +1,16 @@
+using System.Numerics;
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    public Transform Knight; 
+    public Transform Wizard;
+    public Transform Archer;
+    public Transform Selection; 
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            Selection.position = Wizard.position;
+        }
     }
 }
