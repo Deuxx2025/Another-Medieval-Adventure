b3639e546d74a0fc5b961a2361c2405fdcb53aab
Deuxx2025 <cristhianloa15@gmail.com>
2025-12-16 14:28:09 +0000

    Enemy Highlight System functional

--- Assets/_Scripts/Game Manager.cs
+++ Assets/_Scripts/Game Manager.cs
@@ -11,10 +11,15 @@ using UnityEngine.UIElements;
 public class GameManager : MonoBehaviour
 {
     //Transforms
+        //allies
     public Transform Knight;            
     public Transform Wizard;
     public Transform Archer;
     public Transform Selection;
+        //enemies
+    public Transform Engineer;
+    public Transform Scientist;
+    public Transform Warrior;
     public Transform EnemySelection; 
 
 
@@ -27,28 +32,31 @@ public class GameManager : MonoBehaviour
     public int positions;
     public int enemypositions; 
     public bool isActive = false; 
+    public bool isActiveEnemy = false; 
     public bool isAttacking = false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         positions = 1;
+        enemypositions = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         #region Highlight System
+        //Player Highlight System
         if (isActive == true)
         {
             Highlight.SetActive(true);
         }
-        else
+        else                                //Making the SetActive value be handle by a bool
         {
             Highlight.SetActive(false);
         }
 
-        if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame && isAttacking == false)
         {
             isActive = true;
             if (isActive == true)
@@ -57,7 +65,7 @@ public class GameManager : MonoBehaviour
             }
         } 
 
-        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame && isAttacking == false)
         {
             isActive = true; 
             if (isActive == true)
@@ -87,9 +95,54 @@ public class GameManager : MonoBehaviour
             positions = 3; 
         }
 
+        //Enemy Higlight System
+        if (isActiveEnemy == true)
+        {
+            EnemyHighlight.SetActive(true);
+        }
+        else
+        {
+            EnemyHighlight.SetActive(false);
+        }
+
         if (positions == 1 && Keyboard.current.enterKey.wasPressedThisFrame)
         {
             isAttacking = true;
+            if (isAttacking == true)
+            {
+                isActiveEnemy = true;
+            }
+        }
+
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame && isAttacking == true)
+        {
+            enemypositions++;
+        } 
+
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame && isAttacking == true)
+        {
+            enemypositions--;   
+        }
+        
+        if (enemypositions == 1)
+        {
+            EnemySelection.position = Engineer.position;
+        }
+        else if (enemypositions == 2)
+        {
+            EnemySelection.position = Scientist.position;
+        }
+        else if (enemypositions == 3)
+        {
+            EnemySelection.position = Warrior.position; 
+        }
+        else if (enemypositions > 3)
+        {
+            enemypositions = 1;
+        }
+        else if (enemypositions < 1)
+        {
+            enemypositions = 3; 
         }
         #endregion
     }
