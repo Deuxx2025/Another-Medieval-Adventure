acf36651255d149d0802f00fc2651bdf803e7853
Deuxx2025 <cristhianloa15@gmail.com>
2026-04-06 16:41:42 +0100

    HealthDisplay script and logic

---
+++ Assets/_Scripts/Combat/HealthDisplay.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject heartPrefab; 
+    [SerializeField] private Transform heartsContainer; 
+    [SerializeField] private Sprite Hearts;
+
+    private List<Image> _hearts = new List<Image>();
+
+    public void Initialize(int maxHealth)
+    {
+        foreach (Transform child in heartsContainer)
+        {
+            //Destroy(child.GameObject);
+            _hearts.Clear();
+        }
+
+        for (int i = 0; i < maxHealth; i++)
+        {
+            GameObject h = Instantiate(heartPrefab, heartsContainer);
+            _hearts.Add(h.GetComponent<Image>());
+        } 
+    }
+
+    public void UpdateHealth(int currentHealth)
+    {
+        for (int i = 0; i < _hearts.Count; i++)
+        {
+            _hearts[i].gameObject.SetActive(i < currentHealth);
+        }
+    }
+}
